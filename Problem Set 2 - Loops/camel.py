a89483c28e4ca9845e0056d9bb9a344736507d25
SciWilro <mr.wilro@gmail.com>
2022-08-31 12:08:12 +0200

    camel.py

---
+++ Problem Set 2 - Loops/camel.py
@@ -0,0 +1,30 @@
+__author__ = "Wilro - https://github.com/SciWilro"
+
+# prompts for variable name in camel case
+# outputs name in snake case
+# Assume that the user’s input will indeed be in camel case.
+# str methods doc: https://docs.python.org/3/library/stdtypes.html#string-methods
+# Eg
+# camelCase: preferredFirstName
+# snake_case: preferred_first_name
+
+
+def main():
+    usr_input = input("camelCase: ")
+    snake_case = convertCamelToSnake(usr_input)
+    print("snake_case: " + snake_case)
+
+
+def convertCamelToSnake(camel):
+    """Uses list methods - Regex would probably be better though"""
+    snake = [""]
+    for s in camel:
+        if s.isupper():
+            snake.append("_")
+            snake.append(s.lower())
+        else:
+            snake.append(s)
+    return "".join(snake)
+
+
+main()
