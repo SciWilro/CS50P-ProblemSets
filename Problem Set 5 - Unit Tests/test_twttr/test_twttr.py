860d25b1f69da229aaf6a6a0ac33c545002b04da
SciWilro <mr.wilro@gmail.com>
2022-09-20 18:56:51 +0200

    Explanation Added

--- Problem Set 5 - Unit Tests/test_twttr/test_twttr.py
+++ Problem Set 5 - Unit Tests/test_twttr/test_twttr.py
@@ -1,21 +1,27 @@
-__author__ = "Wilro - https://github.com/SciWilro"
+# __author__ = "Wilro - https://github.com/SciWilro"
+
+# Was tricky to work out what to do here
+# But the results of check50 gives the scenarios to check for
+# See the commented lines
+
 import pytest
 
 from twttr import shorten
 
 
-def test_lower():
-    assert shorten("testing") == "tstng"
-
-
-def test_Upper():
+def test_shorten():
+    # catches twttr.py without vowel replacement
+    assert shorten("testING") == "tstNG"
+    # catches twttr.py without capitalized vowel replacement
     assert shorten("TESTING") == "TSTNG"
+    # twttr.py without lowercase vowel replacement
+    assert shorten("testing") == "tstng"
+    # catches twttr.py omitting numbers
+    assert shorten("0Testing") == "0Tstng"
+    # catches twttr.py printing in uppercase
+    assert shorten("testing") == "tstng"
+    # catches twttr.py omitting punctuation
+    assert shorten("!testing!") == "!tstng!"
 
-
-def test_spaces():
-    assert shorten(" TESTING ") == "TSTNG"
-
-
-def test_var_type():
     with pytest.raises(TypeError):
         shorten(5)
